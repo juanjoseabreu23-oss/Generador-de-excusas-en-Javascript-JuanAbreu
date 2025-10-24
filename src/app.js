1d52ad07529f9c9ab14c5200bd243d2a294f4eae
juan jose abreu goncalves <juanjoseabreu23@gmail.com>
2025-10-24 16:12:44 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // Paso 1: Crear los arrays con las partes de la excusa
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+  
+
+  let excuse = who[randomWho] + ' ' + action[randomAction] + ' ' + what[randomWhat] + ' ' + when[randomWhen];
+  
+
+  document.getElementById("excuse").innerHTML = excuse;
+  
+
+  console.log("Excusa generada: " + excuse);
+  };
